e37278aa3a18003558165f9e848f092d9db0d3b9
Duncan Calvert <dcalvert@ihmc.us>
2023-02-28 10:05:20 -0600

    :ihmc-ci: 7.7

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,56 +2,34 @@ import com.gradle.publish.MavenCoordinates
 
 plugins {
    `kotlin-dsl`
-   `java-gradle-plugin`
-   `maven-publish`
-   id("com.gradle.plugin-publish") version "0.18.0"
+   id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 group = "us.ihmc"
-version = "7.6"
+version = "7.7"
 
 repositories {
    mavenCentral()
 }
 
 dependencies {
-   api("org.junit.platform:junit-platform-console:1.8.2")
-   api("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+   api("org.junit.platform:junit-platform-console:1.9.2")
+   api("org.junit.jupiter:junit-jupiter-engine:5.9.2")
    api("com.github.kittinunf.fuel:fuel:2.3.1")
-   api("org.json:json:20211205")
+   api("org.json:json:20220924")
 }
 
-val pluginDisplayName = "IHMC CI"
-val pluginDescription = "Gradle plugin for running groups of tests with varied runtime requirements."
 val pluginVcsUrl = "https://github.com/ihmcrobotics/ihmc-ci"
-val pluginTags = listOf("ci", "continuous", "integration", "ihmc", "robotics")
 
 gradlePlugin {
+   website.set(pluginVcsUrl)
+   vcsUrl.set(pluginVcsUrl)
+
    plugins.register(project.name) {
       id = project.group as String + "." + project.name
       implementationClass = "us.ihmc.ci.IHMCCIPlugin"
-      displayName = pluginDisplayName
-      description = pluginDescription
+      displayName = "IHMC CI"
+      description = "Gradle plugin for running groups of tests with varied runtime requirements."
+      tags.set(listOf("ci", "continuous", "integration", "ihmc", "robotics"))
    }
-}
-
-pluginBundle {
-   website = pluginVcsUrl
-   vcsUrl = pluginVcsUrl
-   description = pluginDescription
-   tags = pluginTags
-
-   plugins.getByName(project.name) {
-      id = project.group as String + "." + project.name
-      version = project.version as String
-      displayName = pluginDisplayName
-      description = pluginDescription
-      tags = pluginTags
-   }
-
-   mavenCoordinates(closureOf<MavenCoordinates> {
-      groupId = project.group as String
-      artifactId = project.name
-      version = project.version as String
-   })
 }
